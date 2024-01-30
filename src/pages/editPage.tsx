aae39d68bb10ce23390bea4d405b3f51fb10e364
nastassiaf1 <nastassia.fiadotava1@gmail.com>
2024-01-30 15:00:15 +0400

    add checkbox & remove unnecessary form state setting

--- src/pages/editPage.tsx
+++ src/pages/editPage.tsx
@@ -1,33 +1,18 @@
 import { useNavigate, useParams } from "react-router-dom";
-import Select from "react-select";
 import { useForm, Controller, SubmitHandler } from "react-hook-form"
 import { useGetRowQuery, useGetTableDataQuery, useUpdateDataMutation } from "../api";
 import Spinner from "../components/spinner";
 import { TableData } from "../interfaces/tableData";
 import { maxLengthName, minLengthName } from "../constants/userTable.const";
-import { useEffect } from "react";
 
 export default function EditPage() {
     const navigate = useNavigate();
     const { rowId } = useParams();
-    const { data, error, isLoading } = useGetRowQuery({ id: Number(rowId) });
-    const { control, handleSubmit, setValue } = useForm({
-        id: rowId,
-        name: null,
-        age: null,
-        isVerified: null
-    });
+    const { data, error, isLoading } = useGetRowQuery({ id: +rowId! });
+    const { control, handleSubmit } = useForm();
 
     const [updateData] = useUpdateDataMutation();
     const getTableDataQuery = useGetTableDataQuery();
-
-    useEffect(() => {
-        if (data) {
-            setValue('name', data.name);
-            setValue('age', data.age);
-            setValue('isVerified', String(data.isVerified));
-        }
-    }, [data, setValue]);
 
     const onSubmit: SubmitHandler<TableData> = async ({ id, name, age, isVerified }) => {
         const updatedData = await updateData({ id: rowId, name, age, isVerified: isVerified.value });
@@ -48,43 +33,48 @@ export default function EditPage() {
     }
 
       return (
+        data ?
         <form onSubmit={handleSubmit(onSubmit)}>
-          <Controller
-            name="name"
-            control={control}
-            rules={{ required: true, maxLength: maxLengthName, minLength: minLengthName }}
-            render={({ field }) => <input defaultValue={data!.name} {...field} />}
-          />
-          <Controller
-            name="age"
-            control={control}
-            rules={{ required: true, min: 17, max: 99 }}
-            render={({ field }) => <input defaultValue={data!.age} type="number" {...field} />}
-          />
+            <Controller
+                name="name"
+                control={control}
+                defaultValue={data?.name || ''}
+                rules={{ required: true, maxLength: maxLengthName, minLength: minLengthName }}
+                render={({ field }) => <input {...field} />}
+            />
+            <Controller
+                name="age"
+                control={control}
+                defaultValue={data?.age || ''}
+                rules={{ required: true, min: 17, max: 99 }}
+                render={({ field }) => <input type="number" {...field} />}
+            />
             <Controller
                 name="isVerified"
                 control={control}
+                defaultValue={data?.isVerified || ''}
                 render={({ field }) => (
-                    <Select
-                        {...field}
-                        options={[
-                            { value: "true", label: "true" },
-                            { value: "false", label: "false"}
-                        ]}
-                        placeholder={String(data!.isVerified)}
-                    />
+                    <div>
+                        <input
+                            type="checkbox"
+                            id="isVerified"
+                            {...field}
+                            checked={field.value}
+                        />
+                        <label htmlFor="isVerified">Verified</label>
+                    </div>
                 )}
             />
-
-          <button type="submit">Save</button>
-          <button
-            type="button"
-            onClick={() => {
-                navigate(-1);
-            }}
-          >
-            Cancel
-          </button>
-        </form>
+            <button type="submit">Save</button>
+            <button
+                type="button"
+                onClick={() => {
+                    navigate(-1);
+                }}
+            >
+                Cancel
+            </button>
+        </form> :
+        <Spinner />
       )
 }
