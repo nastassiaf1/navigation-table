ff87fa21800fcaf8c970c6200387811a0d098a7a
nastassiaf1 <nastassia.fiadotava1@gmail.com>
2024-01-30 15:17:07 +0400

    catch editing error

--- src/pages/editPage.tsx
+++ src/pages/editPage.tsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useForm, Controller, SubmitHandler } from "react-hook-form"
+
 import { useGetRowQuery, useGetTableDataQuery, useUpdateDataMutation } from "../api";
+
 import Spinner from "../components/spinner";
 import { TableData } from "../interfaces/tableData";
 import { maxLengthName, minLengthName } from "../constants/userTable.const";
 
+import errorStyle from './../styles/error.module.scss'
+
+
 export default function EditPage() {
+    const errorMessage = 'Failed to update data. Please try again.';
     const navigate = useNavigate();
     const { rowId } = useParams();
     const { data, error, isLoading } = useGetRowQuery({ id: +rowId! });
     const { control, handleSubmit } = useForm();
+    const [updateError, setUpdateError] = useState<string | null>(null);
 
     const [updateData] = useUpdateDataMutation();
     const getTableDataQuery = useGetTableDataQuery();
@@ -17,11 +25,15 @@ export default function EditPage() {
     const onSubmit: SubmitHandler<TableData> = async ({ id, name, age, isVerified }) => {
         const updatedData = await updateData({ id: rowId, name, age, isVerified: isVerified.value });
 
-        if (!updatedData.error) {
-            await getTableDataQuery.refetch();
+        if (updatedData.error) {
+            setUpdateError(errorMessage);
 
-            navigate('/table');
+            return;
         }
+
+        await getTableDataQuery.refetch();
+
+        navigate('/table');
     }
 
     if (isLoading) {
@@ -34,47 +46,48 @@ export default function EditPage() {
 
       return (
         data ?
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <Controller
-                name="name"
-                control={control}
-                defaultValue={data?.name || ''}
-                rules={{ required: true, maxLength: maxLengthName, minLength: minLengthName }}
-                render={({ field }) => <input {...field} />}
-            />
-            <Controller
-                name="age"
-                control={control}
-                defaultValue={data?.age || ''}
-                rules={{ required: true, min: 17, max: 99 }}
-                render={({ field }) => <input type="number" {...field} />}
-            />
-            <Controller
-                name="isVerified"
-                control={control}
-                defaultValue={data?.isVerified || ''}
-                render={({ field }) => (
-                    <div>
-                        <input
-                            type="checkbox"
-                            id="isVerified"
-                            {...field}
-                            checked={field.value}
-                        />
-                        <label htmlFor="isVerified">Verified</label>
-                    </div>
-                )}
-            />
-            <button type="submit">Save</button>
-            <button
-                type="button"
-                onClick={() => {
-                    navigate(-1);
-                }}
-            >
-                Cancel
-            </button>
-        </form> :
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Controller
+                    name="name"
+                    control={control}
+                    defaultValue={data?.name || ''}
+                    rules={{ required: true, maxLength: maxLengthName, minLength: minLengthName }}
+                    render={({ field }) => <input {...field} />}
+                />
+                <Controller
+                    name="age"
+                    control={control}
+                    defaultValue={data?.age || ''}
+                    rules={{ required: true, min: 17, max: 99 }}
+                    render={({ field }) => <input type="number" {...field} />}
+                />
+                <Controller
+                    name="isVerified"
+                    control={control}
+                    defaultValue={data?.isVerified || ''}
+                    render={({ field }) => (
+                        <div>
+                            <input
+                                type="checkbox"
+                                id="isVerified"
+                                {...field}
+                                checked={field.value}
+                            />
+                            <label htmlFor="isVerified">Verified</label>
+                        </div>
+                    )}
+                />
+                <button type="submit">Save</button>
+                <button
+                    type="button"
+                    onClick={() => {
+                        navigate(-1);
+                    }}
+                >
+                    Cancel
+                </button>
+                {updateError && <div className={errorStyle.error}>Error updating data: {updateError}</div>}
+            </form> :
         <Spinner />
       )
 }
