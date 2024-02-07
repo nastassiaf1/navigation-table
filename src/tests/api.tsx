a54b6d7339caed63c32dc69c6cea65c741a319f3
nastassiaf1 <nastassia.fiadotava1@gmail.com>
2024-02-07 18:57:10 +0400

    add api tests

---
+++ src/tests/api.tsx
@@ -0,0 +1,108 @@
+import { renderHook, waitFor, act } from '@testing-library/react';
+import fetchMock from 'jest-fetch-mock';
+import mockData from 'mocks/data';
+import {
+    useGetTableDataQuery,
+    useUpdateDataMutation,
+    useAddDataMutation,
+    useRemoveDataMutation,
+} from 'api';
+import store from 'store/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { TableData } from 'interfaces/tableData';
+
+fetchMock.enableMocks();
+
+const wrapper = ({ children }: any) => {
+    return <Provider store={store}><BrowserRouter>{children}</BrowserRouter></Provider>;
+};
+
+beforeAll((): void => {
+    fetchMock.resetMocks();
+});
+
+describe('tableApi', () => {
+    const newData: TableData = { id: 'test', name: 'Test', age: 33, isVerified: true };
+
+    it('renders useGetTableDataQuery', async () => {
+        fetchMock.mockResponse(JSON.stringify(mockData));
+        const { result } = renderHook(() => useGetTableDataQuery(), {
+            wrapper
+        });
+
+        expect(result.current.isLoading).toBe(true);
+
+        await waitFor(() => expect(result.current.isLoading).toBe(false));
+        await waitFor(() => expect(result.current.data).toBeDefined());
+    });
+
+    it('renders useUpdateDataMutation', async () => {
+        const updatedUser = { ...mockData[0], name: 'Test' };
+
+        fetchMock.mockResponse(JSON.stringify(updatedUser));
+
+        const { result } = renderHook(
+            () => useUpdateDataMutation(undefined),
+            { wrapper }
+        );
+        const [updateData, initialResponse] = result.current;
+
+        expect(initialResponse.data).toBeUndefined();
+        expect(initialResponse.isLoading).toBe(false);
+
+        act(() => {
+            void updateData(updatedUser);
+        });
+
+        const loadingResponse = result.current[1];
+        expect(loadingResponse.data).toBeUndefined();
+        expect(loadingResponse.isLoading).toBe(true);
+
+        await waitFor(() => expect(result.current[1].isLoading).toBe(false));
+        await waitFor(() => expect(result.current[1].data).toBeDefined());
+        await waitFor(() => expect(result.current[1].isSuccess).toBe(true));
+    });
+
+    it('add new data by useAddDataMutation', async () => {
+        fetchMock.mockResponse(JSON.stringify(newData));
+
+        const { result } = renderHook(
+            () => useAddDataMutation(undefined),
+            { wrapper }
+        );
+        const [addNewData, initialResponse] = result.current;
+
+        expect(initialResponse.data).toBeUndefined();
+
+        act(() => {
+            void addNewData(newData);
+        });
+
+        await waitFor(() => expect(result.current[1].data).toBeDefined());
+
+        const savedData = store.getState().api.queries['getTableData(undefined)']?.data || [];
+        expect((savedData as any[]).find(item => item.id === newData.id)).toBeDefined();
+    });
+
+    it('remove new data by useAddDataMutation', async () => {
+        fetchMock.mockResponse(JSON.stringify(newData));
+
+        const { result } = renderHook(
+            () => useRemoveDataMutation(undefined),
+            { wrapper }
+        );
+        const [removeNewData, initialResponse] = result.current;
+
+        expect(initialResponse.data).toBeUndefined();
+
+        act(() => {
+            void removeNewData(newData.id!);
+        });
+
+        await waitFor(() => expect(result.current[1].data).toBeDefined());
+
+        const savedData = store.getState().api.queries['getTableData(undefined)']?.data || [];
+        expect((savedData as any[]).find(item => item.id === newData.id)).toBeUndefined();
+    });
+});
