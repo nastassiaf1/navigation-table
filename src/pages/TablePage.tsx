9195893cfb3638fb3847c2d3db55354212f7ff62
nastassiaf1 <nastassia.fiadotava1@gmail.com>
2024-02-12 16:35:11 +0400

    add not found user state

--- src/pages/TablePage.tsx
+++ src/pages/TablePage.tsx
@@ -28,34 +28,38 @@ export default function TablePage() {
     await removeData(id);
   }
 
-  return <div>
+  return <div className={styles.tablewrapper}>
     <Filter disabled={!!data} setData={setData} data={initialData || []}/>
 
-    <table className={styles.table}>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Verification</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            {data?.map((item) => (
-                <tr key={item.id}>
-                    <td>{item.id}</td>
-                    <td>{item.name}</td>
-                    <td>{item.age}</td>
-                    <td>{`${item.isVerified}`}</td>
-                    <td>
-                        <Link to={`/table/${item.id}/edit`} aria-label={`Edit user ${item.name}, id ${item.id}`}>Edit</Link>
-                        <button className={`${styles.button} ${styles.itembutton}`} aria-label={`Remove user ${item.name}, id ${item.id}`} onClick={() => onDeleteUser(item.id!)}>Delete</button>
-                    </td>
-                </tr>
-            ))}
-        </tbody>
-    </table>
+    { data.length ?
+      <table className={styles.table}>
+          <thead>
+              <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Age</th>
+                  <th>Verification</th>
+                  <th></th>
+              </tr>
+          </thead>
+          <tbody>
+              { data.map((item) => (
+                  <tr key={item.id}>
+                      <td>{item.id}</td>
+                      <td>{item.name}</td>
+                      <td>{item.age}</td>
+                      <td>{`${item.isVerified}`}</td>
+                      <td>
+                          <Link to={`/table/${item.id}/edit`} aria-label={`Edit user ${item.name}, id ${item.id}`}>Edit</Link>
+                          <button className={`${styles.button} ${styles.itembutton}`} aria-label={`Remove user ${item.name}, id ${item.id}`} onClick={() => onDeleteUser(item.id!)}>Delete</button>
+                      </td>
+                  </tr>
+                ))
+              }
+          </tbody>
+      </table> :
+      <p className={styles.description}>Users not found</p>
+    }
     <Link to={`/table/add`} className={styles.button} aria-label="Add new user">
         Add
     </Link>
