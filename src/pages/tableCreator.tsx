b4ff27e990656839ea792a480b32d5383c2fe116
nastassiaf1 <nastassia.fiadotava1@gmail.com>
2024-02-14 23:20:44 +0400

    add table creator

---
+++ src/pages/tableCreator.tsx
@@ -0,0 +1,5 @@
+export default function TableCreator() {
+    return <div className='wrapper'>
+        <button>Create table</button>
+    </div>
+};
--- src/router/guards/table.tsx
+++ src/router/guards/table.tsx
@@ -1,15 +1,15 @@
-import { Navigate, useLocation } from 'react-router-dom';
 import { ReactNode } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from 'store/store';
 
 export default function RequireAuth({ children }: { children: ReactNode }) {
-  const location = useLocation();
-  const user = useSelector((state: RootState) => state.user.currentUser);
+    const location = useLocation();
+    const user = useSelector((state: RootState) => state.user.currentUser);
 
-  if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
+    if (!user) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
 
-  return children;
+    return children;
 };
